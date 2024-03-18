bca15dcc0f7e18ea40647640d0434d225fd74325
nikhil061102 <106477217+nikhil061102@users.noreply.github.com>
2024-03-19 01:50:11 +0530

    learnt 2

--- index.js
+++ index.js
@@ -15,12 +15,25 @@ const server = app.listen(PORT, () => {
 });
 
 const io = socket(server);
+const maxCap = 3;
 io.on('connection', (socket) => {
-  console.log('made socket connection', socket.id);
-  socket.on('join', function(data){
-    io.sockets.emit('chat', data);
+  console.log(socket.id,'connected');
+  const rooms = io.sockets.adapter.rooms;
+  console.log(rooms) // this lists all the rooms
+  socket.on('join', (roomName) => {
+    if(rooms.get(roomName) === undefined){
+      socket.join(roomName);
+      console.log('room created', roomName);
+    } else if (rooms.get(roomName).size < maxCap){
+      socket.join(roomName);
+      console.log('room joined', roomName);
+    } else {
+      console.log('room is full');
+    }
+    console.log(rooms)
   });
-  // socket.on('typing', function(data){
-  //   socket.broadcast.emit('typing', data);
-  // });
+  socket.on('disconnect', () => {
+    console.log(socket.id,'disconnected');
+    console.log(rooms);
+  });
 });
